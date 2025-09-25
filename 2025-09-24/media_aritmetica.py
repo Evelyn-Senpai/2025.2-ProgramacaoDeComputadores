5611ce187ba79d104782042be0fe3962f56fe300
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-09-25 09:05:29 -0300

    exercicio da média aritmética

---
+++ 2025-09-24/media_aritmetica.py
@@ -0,0 +1,5 @@
+#03Desenvolva um programa em Python que solicite ao usuário dois valores e em seguida calcule a média aritmética entre eles.
+v1 = int(input('Digite o primeiro valor valor: '))
+v2 = int(input('Digite o segundo valor valor: '))
+media = (v1+v2)/2
+print(f'A média é = {media}')
