0710487fc8832f902fcfb4ea0050b0c746bafb64
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-09-25 11:16:55 -0300

    treze exercícios sobre a aula03

---
+++ CursoPython/exerciciosaula03.py
@@ -0,0 +1,60 @@
+# Faça um programa que leia um número inteiro e mostre na telao seu sucessor e seu antecessor.
+# n1 = int(input('Digite um valor: '))
+# suc = n1+1
+# ant = n1-1
+# print('O antecessor desse número é: {}\nO sucessor desse número é: {}'.format(ant,suc))
+
+# Crie um algoritmo que leia um número e mostre o seu dobro, tríplo e raiz quadrada.
+# n1 = int(input('Digite um valor: '))
+# dob = n1*2
+# trip = n1*3
+# raq = n1**(1/2)
+# print('O dobro deste número é: {}\nO tríplo deste valor é: {}\nA raiz quadrada deste valor é: {:.2f}'.format(dob,trip,raq))
+
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+# n1 = float(input('Digite a primeira nota: '))
+# n2 = float(input('Digite a segunda nota: '))
+# media = (n1+n2)/2
+# print('A média é: {:.1f}'.format(media))
+
+# Escreve um programa que leia um valor em metros e o exiba convertido em centrímetros e milímetros.
+# v1 = int(input('Digite um valor em metros: '))
+# cm = v1*100
+# mm = v1*1000
+# print('Metros = {:.0f}\nCentrímetros = {:.0f}\nMilímetros = {}'.format(v1,cm,mm))
+
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
+# n = int(input('Digite um número: '))
+# print('Sua tabuada é: ')
+# print('{} x {} = {}'.format(n, 1, n*1))
+# print('{} x {} = {}'.format(n, 2, n*2))
+# print('{} x {} = {}'.format(n, 3, n*3))
+# print('{} x {} = {}'.format(n, 4, n*4))
+# print('{} x {} = {}'.format(n, 5, n*5))
+# print('{} x {} = {}'.format(n, 6, n*6))
+# print('{} x {} = {}'.format(n, 7, n*7))
+# print('{} x {} = {}'.format(n, 8, n*8))
+# print('{} x {} = {}'.format(n, 9, n*9))
+
+# Crie um programa que leia quanto dinheiro uma pesssoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere USS1.00 = R$3.27
+# d = float(input('Digite quanto dinheiro você tem: R$'))
+# con = d/3.27
+# print('Você pode comprar USS{:.2f}'.format(con)) 
+
+# Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinte para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m²
+# l = float(input('Digite a largura da parede: '))
+# a = float(input('Digite a altura da parede: '))
+# area = l*a
+# tint = area/2
+# print('A área da sua parede é: {:.1f} m² e é necessario {:.1f} litros de tinta para pintá-la.'.format(area,tint))
+
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+# p = float(input('Digite o preço do produto: R$'))
+# desc = (5*p)/100
+# print('O produto com 5% de desconto fica: R${:.2f}'.format(p-desc))
+
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+# sal = float(input('Digite seu salário: R$'))
+# aum = (15*sal)/100
+# salaum = aum+sal
+# print('Com 15% de aumento fica: RS{:.2f}'.format(salaum))
