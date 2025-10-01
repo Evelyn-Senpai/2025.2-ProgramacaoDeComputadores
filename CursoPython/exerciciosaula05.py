e0f284288a3cbc0408ad5e6f0ee38d4516dd60a8
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-10-01 15:57:09 -0300

    cinco exercícios sobre a aula05

---
+++ CursoPython/exerciciosaula05.py
@@ -0,0 +1,25 @@
+#  Crie um programa que leia o nome completo de uma pessoa e mostre: - O nome com todas as letras maiúsculas e minúsculas. - Quantas letras ao todo (sem considerar espaços). - Quantas letras tem o primeiro nome.
+# nome = str(input('Digite seu nome completo: '))
+# print('Seu nome com todas as letras maiúsculas: {}'.format(nome.upper()))
+# print('Seu nome com todas as letras miúsculas: {}'.format(nome.lower()))
+# print('A quantidade de letras no seu nome é: {}'.format(len(nome)))
+# print('A quantidade de letras no seu primeiro nome é: {}'.format(nome[0:' ']))
+
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+# n1 = int(input('Digite um número entre 0 e 9999: '))
+# print('-'.join(n1))
+
+
+# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+# cidade = str(input('Digite o nome de uma cidade: '))
+# print('Essa cidade começa com o nome Santo? {}'.format('Santo' in cidade))
+
+
+# Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+# frase = str(input('Digite uma frase: '))
+# print('A quantidade de vezes que aparece a letra "a" é: {}'.format(frase.count('a')))
+# print('Na posição {} pela primeira vez e na {} pela última vez: '.format(frase.find('a'), ))
+
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+# nome = str(input('Digite seu nome completo: '))
+# print(nome.split())
