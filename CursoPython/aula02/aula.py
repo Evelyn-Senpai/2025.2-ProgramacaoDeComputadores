6081190f6ea082d2d29db753746ae2a3e9bafc80
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-10-06 14:05:53 -0300

    tipos primitivos e format

---
+++ CursoPython/aula02/aula.py
@@ -0,0 +1,20 @@
+#Números inteiros
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma entre', n1, '+',n2,'vale:',soma)
+print('A soma entre {} e {} vale: {}'.format(n1,n2,soma))
+
+#Númeors flutuantes
+n1 = float(input('Digite um número: '))
+n2 = float(input('Dgite outro número: '))
+soma = n1 + n2
+print('A soma entre {} e {} vale: {}'.format(n1,n2,soma))
+
+#Booleanos
+n = bool(input('Digite alguma coisa: '))
+print(n)
+
+#Strings
+nome = input('Digite seu nome: ')   
+print('Olá,', nome)
