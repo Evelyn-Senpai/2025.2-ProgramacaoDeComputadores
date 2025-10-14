7beae921b9c0c144680538e0fcb7c6cd78f2d6aa
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-10-14 16:42:04 -0300

    contagem de pares e impares

---
+++ CursoPython/aula08/exercicio02.py
@@ -0,0 +1,20 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+# n = 0
+
+for n in range(2, 51, 2):
+# for n in range(n, 50+1):
+
+    # resto = n%2
+
+    # if resto == 0:
+    #     print('{} é par'.format(n))
+    # else:
+    #     print('{} é impar'.format(n))
+
+    # if resto == 0:
+        print(n, end='\n')
+    
+    # n += 1
+    
+print('Fim')
