df1f619e28beee560f455e32867648dab60112e4
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-10-16 17:33:16 -0300

    maior e menor da sequencia

--- CursoPython/aula08/exercicio10.py
+++ CursoPython/aula08/exercicio10.py
@@ -1,18 +1,21 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-tot_maior = 0
-tot_menor = 0
+maior = 0
+menor = 0
 peso = 0
 
 for pessoa in range(1, 5+1):
     peso = float(input('Qual o peso da {}ª pessoa? '.format(pessoa)))
 
-    if peso >= peso:
-        tot_maior += 1
+    if pessoa == 1:
+        maior = peso
+        menor = peso
     
     else:
-        tot_menor += 1
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
 
-    peso = peso
-
-print(tot_maior, tot_menor)      
+print('O maior peso é \033[32m{}kg'.format(maior))
+print('\033[mO menor peso é \033[31m{}kg'.format(menor))      
