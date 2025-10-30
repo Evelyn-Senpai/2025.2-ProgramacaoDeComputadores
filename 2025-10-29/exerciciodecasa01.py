eb649c7996f82651e0248861a3a7f1e31b096aac
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-10-29 21:49:55 -0300

    exercício para casa

---
+++ 2025-10-29/exerciciodecasa01.py
@@ -0,0 +1,9 @@
+'''
+   Fazer um programa que solicite um número inteiro (n) e exiba os 
+   n primeiros elementos da sequência de Fibonacci.
+
+   Exemplo: n = 10
+
+   Saída: 
+      1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+'''
