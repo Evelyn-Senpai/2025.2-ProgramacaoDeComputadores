5a9af31b90f9590607f207cc3eff63bd4de3e928
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-10-31 16:14:32 -0300

    sequência de fibonacci com while

--- 2025-11-03/exerciciodecasa03.py
+++ 2025-11-03/exerciciodecasa03.py
@@ -7,3 +7,23 @@
    Saída: 
       1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 '''
+import sys
+try:
+   n = int(input('Informe um número inteiro: '))
+   if n <= 0:
+      sys.exit('ERRO: Informe um número maior que 0!')
+except ValueError:
+   sys.exit('ERRO: Informe um número inteiro!')
+except Exception as Erro:
+   sys.exit(f'ERRO: {Erro}')
+else:
+   f1 = 0
+   f2 = 1
+   cont = 3
+   print(f'{f2}', end='')
+   while cont <= n+1:
+      f = f1 + f2
+      print(f', {f}', end='')
+      f1 = f2
+      f2 = f
+      cont += 1
