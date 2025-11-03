b15412e42dabd0100fdc51b4b5c316a810b1cba3
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-03 15:11:05 -0300

    jogo ímpar ou par

--- CursoPython/aula10/exercicio03.py
+++ CursoPython/aula10/exercicio03.py
@@ -2,25 +2,38 @@
 Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele consquistou no final do jogo.
 '''
 import random
-# while True:
+cont = 0
 print('-='*20)
 print('VAMOS JOGAR PAR OU ÍMPAR')
 print('-='*20)
-valor_jogador = int(input('Diga um valor: '))
-par_ou_impar = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
-print('-'*20)
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-valor_computador = random.choice(lista)
-soma = valor_jogador + valor_computador
-print(f'Você jogou {valor_jogador} e o computador {valor_computador}. Total de {soma} DEU ', end='')
-print('-'*20)
-if soma % 2 == 0:
-    print('PAR')
-else:
-    print('ÍMPAR')
-if par_ou_impar in 'Pp':
-    print('Você GANHOU!')
+while True:
+    valor_jogador = int(input('Diga um valor: '))
+    par_ou_impar = ' '
+    while par_ou_impar not in 'PI':
+        par_ou_impar = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print('-'*20)
+    valor_computador = random.randint(0, 10)
+    soma = valor_jogador + valor_computador
+    print(f'Você jogou {valor_jogador} e o computador {valor_computador}. Total de {soma} DEU ', end = '')
+    if par_ou_impar == 'P':
+        print('PAR')
+        print('-'*20)
+        if soma % 2 == 0:
+            print('Você GANHOU!')
+            cont += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif par_ou_impar == 'I':
+        print('ÍMPAR')
+        print('-'*20)
+        if soma % 2 == 1:
+            print('Você GANHOU!')
+            cont += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print('Vamos jogar novamente...')
     print('-='*20)
-else:
-    print('Você PERDEU!')
-    print('-='*20)
+print('-='*20)
+print(f'GAME OVER! Você venceu {cont} vezes.')
