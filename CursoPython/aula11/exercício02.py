37c94f033460d02ded4ce80af8a923de3213702e
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-04 17:46:33 -0300

    tuplas com times de futebol

---
+++ CursoPython/aula11/exercício02.py
@@ -0,0 +1,11 @@
+'''
+Crie uma tupla preenchida com os 10 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+a) Apenas os 5 primeiros colocados.
+b) Os últimos 4 colocados da tabela.
+c) Uma lista com os times em ordem alfabética.
+d) Em que posição na tabela está o time da Chapecoense.
+'''
+times = ('Palmeiras', 'Flamengo', 'Mirassol', 'Bahia', 'Botafogo', 'Fluminense', 'São Paulo', 'Vasco Gama', 'Corinthians', 'Gremio')
+print('-='*15)
+print('Lista de times do Brasileirão:')
+print('-='*15)
