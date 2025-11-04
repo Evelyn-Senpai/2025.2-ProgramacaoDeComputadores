3a78501d5a48653227d616d3dcfbf4f64152f2df
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-04 16:57:27 -0300

    váriaveis compostas(tuplas)

---
+++ CursoPython/aula11/aula.py
@@ -0,0 +1,43 @@
+'''
+TUPLAS SÃO IMUTÁVEIS
+''' 
+# lanche = ('hambúrger')
+# print(lanche)
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim')
+# print(lanche[2])
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim')
+# print(lanche[-2])
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim')
+# print(lanche[1:3])
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim')
+# print(len(lanche))
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim')
+# for c in lanche:
+#     print(c)
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim', 'Batata Frita')
+# for cont in range(0, len(lanche)):
+#     print(lanche[cont])
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim', 'Batata Frita')
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+
+# lanche = ('Suco', 'Hambúrger', 'Pizza', 'Pudim', 'Batata Frita')
+# print(sorted(lanche))
+
+# a = (0, 1, 2, 3, 4)
+# b = (5, 6, 7, 8, 9)
+# c = a + b
+# print(c)
+# print(c.count(5))
+# print(c.index(3, 1))
+
+pessoa = ('Evelyn', 20, 'F', 60.0)
+del(pessoa)
+print(pessoa)
