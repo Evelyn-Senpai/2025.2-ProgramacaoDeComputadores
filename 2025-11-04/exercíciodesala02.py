38519c355ff8fe502c111d4dd9512eafd9a7bd37
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-05 21:38:45 -0300

    número triangular com exeções

--- 2025-11-04/exercíciodesala02.py
+++ 2025-11-04/exercíciodesala02.py
@@ -7,6 +7,7 @@
 
    Os primeiros números triangulares são: 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
 '''
+from random import triangular
 import sys
 try:
     numero = int(input('Informe um valor inteiro: '))
@@ -14,6 +15,19 @@ try:
         sys.exit('ERRO: O valor informado deve ser maior que 0!')
 except ValueError:
     sys.exit('ERRO: O valor informado deve ser inteiro!')
+except KeyboardInterrupt:
+    sys.exit('ERRO: Ctrl + C pressionado')
 except Exception as Erro:
     sys.exit(f'ERRO: {Erro}')
 else:
+    if numero < 1:
+        sys.exit(f'{numero} não é triangular')
+    triangular = 0
+    incremento = 1
+    while triangular < numero:
+        triangular += incremento
+        incremento += 1
+    if triangular == numero:
+        print(f'\033[0;32mnúmero {numero} é triangular')
+    else:
+        print(f'\033[0;31mO número {numero} não é triangular')
