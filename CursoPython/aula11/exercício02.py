13e11f01ebacf35fbd03c44e9073571792040e68
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-06 17:46:56 -0300

    tuplas com times de futebol

--- CursoPython/aula11/exercício02.py
+++ CursoPython/aula11/exercício02.py
@@ -5,10 +5,11 @@ b) Os últimos 4 colocados da tabela.
 c) Uma lista com os times em ordem alfabética.
 d) Em que posição na tabela está o time da Chapecoense.
 '''
-times = ('Palmeiras', 'Flamengo', 'Mirassol', 'Bahia', 'Botafogo', 'Fluminense', 'São Paulo', 'Vasco Gama', 'Corinthians', 'Gremio')
+times = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Mirassol', 'Bahia', 'Botafogo', 'Fluminense', 'São Paulo', 'Vasco Gama', 'Corinthians', 'Gremio')
 print('-='*15)
 print(f'Lista de times do Brasileirão: {times}')
 print('-='*15)
 print(f'Os primeiros 5 colocados são: {times[0:5]}')
-print(f'Os últimos 4 colocados são: {times[6:10]}')
+print(f'Os últimos 4 colocados são: {times[-4:]}')
 print(f'Lista em ordem alfabética: {sorted(times)}')
+print(f'O Cruzeiro está na {times.index("Cruzeiro")+1} posição')
