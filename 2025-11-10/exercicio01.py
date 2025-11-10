45369b470e67efc62c84f25c103dac6a799e9d3e
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-10 20:16:30 -0300

    "explicação de strings"

--- 2025-11-10/exercicio01.py
+++ 2025-11-10/exercicio01.py
@@ -1,3 +1,15 @@
+'''
+String -> Cadeias de caracters
+       -> Estrutura de dados homogênea
+       -> Cada caracters possui uma posição (índice = index)
+'''
+strNome = 'Rio Grande do Norte'
+print(strNome)
+print(strNome[8])
+print(strNome[-3])
+print(strNome[1:5])
+print(strNome[:10])
+print(strNome[10:])
 strSenha='123Mudar'
 strEntrada=input('Digite a senha')
 if strEntrada == strSenha:
