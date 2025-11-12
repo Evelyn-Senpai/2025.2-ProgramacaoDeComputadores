292bef0c748a41fd6fa69357b4b306c7b04850cd
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-11 21:30:59 -0300

    "explicação" de string

---
+++ 2025-11-11/exercíciodesala.py
@@ -0,0 +1,11 @@
+strTexto = input('Digite algo: ').split()
+# for letra in strTexto:
+#     if letra != ' ':
+#         print(letra, end='')
+#     else:
+#         print('')
+
+#     print(letra, end='' if letra != ' ' else '\n')
+
+for palavra in strTexto:
+    print(palavra)
