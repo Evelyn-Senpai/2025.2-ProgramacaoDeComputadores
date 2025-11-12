d8b52f281b56f561b032721a12f6fcb4a62a20ac
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-11 21:50:09 -0300

    "explicação" de string

---
+++ 2025-11-11/exercíciodesala03.py
@@ -0,0 +1,13 @@
+strTexto = input('Digite uma palavra: ')
+# Se houver ' ', sair do programa
+# Imprima a palavra invertida
+if ' ' in strTexto:
+    print('Você digitou um espaço na palavra, fim do programa')
+else:
+    for letra in (len()):
+        print(letra, end='')
+    # if strTexto.upper() == strTexto_invertido.upper():
+    #     print(f'{strTexto} de trás para frente é {strTexto_invertido}')
+    #     print('Então ele é um Palíndromo')
+    # else:
+    #     print('A palavra não é um Palíndromo')
