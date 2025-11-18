ee7bf4c8db1d339ca0967a1bc2413988dae712cd
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-11-18 14:31:54 -0300

    Reapply "push"
    This reverts commit bc22c4017e6fb2d27a675cd49216673556868f55.

--- Curso Python/aula01/exercicio01.py
+++
@@ -1,5 +0,0 @@
-#Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas com o valor digitado.
-
-nome = input("Qual é o seu nome? ")
-print(nome,"seja bem-vinda(o)")
-
