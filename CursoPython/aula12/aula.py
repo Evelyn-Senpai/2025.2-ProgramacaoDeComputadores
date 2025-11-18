7b7390cb4b4d0103815509adf00db386d467e2fb
Evelyn-Senapi <128evelynaraujo@gmail.com>
2025-11-18 15:33:24 -0300

    VARIÁVEIS COMPOSTAS (LISTAS PARTE 01)

--- CursoPython/aula12/aula.py
+++ CursoPython/aula12/aula.py
@@ -1,32 +1,32 @@
 '''
+VARIÁVEIS COMPOSTAS (LISTAS PARTE 01)
 LISTAS SÃO MUTÁVEIS
 ''' 
-# num = [2, 5, 9, 1]
-# num[3] = 0 # Recebe valores
-# num.append(7) # Adiciona valores
-# num.sort() # Coloca em ordem
-# num.sort(reverse=True) # Reverso da ordem
-# num.insert(2, 1) # Insere um valor na posição que quiser
-# num.pop() # Elimina o último valor
-# num.pop(2) # Elimina o valor na posição
-# num.remove(9) # Elimina o valor
-# print(num)
-# print(f'Essa lista tem {len(num)} elementos')
-# if 9 in num:
-#     num.remove(9)
-#     print(num)
-# else:
-#     print('Não tem o número 9 na lista')
+num = [2, 5, 9, 1]
+num[3] = 0 # Recebe valores
+num.append(7) # Adiciona valores
+num.sort() # Coloca em ordem
+num.sort(reverse=True) # Reverso da ordem
+num.insert(2, 1) # Insere um valor na posição que quiser
+num.pop() # Elimina o último valor
+num.pop(2) # Elimina o valor na posição
+num.remove(9) # Elimina o valor
+print(num)
+print(f'Essa lista tem {len(num)} elementos')
+if 9 in num:
+    num.remove(9)
+    print(num)
+else:
+    print('Não tem o número 9 na lista')
 
-# valores = []
-# valores.append(5)
-# valores.append(9)
-# valores.append(4)
-# for c, v in enumerate(valores):
-#     print(f'Na posição {c} encontrei o valor {v}')
-# print('Fim')
+valores = []
+valores.append(5)
+valores.append(9)
+valores.append(4)
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}')
+print('Fim')
 
-# valores = []
-# for cont in range(0, 5):
-#     valores.append(int(input('Digite um valor: ')))
-
+valores = []
+for cont in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
