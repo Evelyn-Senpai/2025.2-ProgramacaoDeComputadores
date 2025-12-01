621782797f17be74ee09a6f49b14cc9612944d0b
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-12-01 19:50:13 -0300

    feito

--- 2025-12-01/exercíciodecasa02.py
+++ 2025-12-01/exercíciodecasa02.py
@@ -30,18 +30,32 @@ lstNomes = list()
 lstNotas1 = list()
 lstNotas2 = list()
 lstMedias = list()
-for i in range(1):
-    nome = str(input('Digite o nome do aluno: '))
-    nota1 = float(input('Digite a nota da etapa 1: '))
-    nota2 = float(input('Digite a nota da etapa 2: '))
-    lstNomes.append(nome)
-    lstNotas1.append(nota1)
-    lstNotas2.append(nota2)
-    media = ((nota1 * 2) + (nota2 * 3)) / 5
-    lstMedias.append(media)
+for i in range(5):
+   nome = str(input('Digite o nome do aluno: '))
+   try:
+      nota1 = float(input('Digite a nota da etapa 1: '))
+      nota2 = float(input('Digite a nota da etapa 2: '))
+      if nota1 < 0 or nota1 > 100:
+         print('ERRO: A nota da etapa 1 deve estar entre 0 e 100!')
+         continue
+      elif nota2 < 0 or nota2 > 100:
+         print('ERRO: A nota da etapa 2 deve estar entre 0 e 100!')
+         continue
+   except ValueError:
+      print('ERRO: Valor incorreto!')
+      continue
+   except Exception as ERRO:
+      print(f'ERRO: {ERRO}')
+      continue
+   else:
+      lstNomes.append(nome)
+      lstNotas1.append(nota1)
+      lstNotas2.append(nota2)
+      media = ((nota1 * 2) + (nota2 * 3)) / 5
+      lstMedias.append(media)
 print('-='*30)
 print('Nome do Aluno       Etapa 1     Etapa 2        Média')
 print('-'*60)
-for a in range(1):
-    print(f'{lstNomes[a]}          {lstNotas1[a]:<10}     {lstNotas2[a]:<10}     {lstMedias[a]}')
-print('-='*30)
+for a in range(5):
+   print(f'{lstNomes[a]}          {lstNotas1[a]:<10}     {lstNotas2[a]:<10}     {lstMedias[a]}')
+   print('-='*30)
