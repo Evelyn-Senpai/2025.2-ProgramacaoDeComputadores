e26c36440c3fe7b4b709f4000a70fce5d484c801
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-12-03 21:34:11 -0300

    "explicação" de arquivos

---
+++ 2025-12-03/exemplo02.py
@@ -0,0 +1,11 @@
+valores = list()
+for i in range(1, 21):
+    x = 1
+    for j in range(1, 1 + i):
+        x *= j
+    valores.append([i, x])
+print(valores)
+arqSaida = open('fatoriais02.csv', 'w')
+for lista in valores:
+    arqSaida.write(f'{lista[0]};{lista[1]}')
+arqSaida.close()
