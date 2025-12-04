7625a1a768ea96b1de6a1494ffa2302af256880b
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-12-03 21:42:44 -0300

    "explicação" de fatoriais

--- 2025-12-03/exemplo02.py
+++ 2025-12-03/exemplo02.py
@@ -7,5 +7,5 @@ for i in range(1, 21):
 print(valores)
 arqSaida = open('fatoriais04.csv', 'a')
 for lista in valores:
-    arqSaida.write(f'{lista[0]};{lista[1]}\n')
+    arqSaida.write(f'\n{lista[0]};{lista[1]}\n')
 arqSaida.close()
