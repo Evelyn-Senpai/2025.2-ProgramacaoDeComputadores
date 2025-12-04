27fd09806ce173bf3614027f881b2e2b0a2371a6
Evelyn-Senpai <128evelynaraujo@gmail.com>
2025-12-03 22:02:00 -0300

    "explicação" de arquivos

--- 2025-12-03/exemplo02.py
+++ 2025-12-03/exemplo02.py
@@ -7,7 +7,7 @@ for i in range(1, 21):
     valores.append([i, x])
 print(valores)
 nomeDir = os.path.dirname(__file__)
-nomeArquivo = f'{nomeDir}fatoriais04.csv'
+nomeArquivo = f'{nomeDir}\\fatoriais04.csv'
 arqSaida = open(nomeArquivo, 'w')
 for lista in valores:
     arqSaida.write(f'{lista[0]};{lista[1]}\n')
